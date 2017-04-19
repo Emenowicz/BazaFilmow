30f4936faa694af0db7ea8fd6f49a315d05692a9
Emenowicz <dmichalowicz96@gmail.com>
2017-04-19 19:25:31 +0200

    Main layout, adapter layout

---
+++ app/src/main/java/com/example/dawidmichalowicz/bazafilmow/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.dawidmichalowicz.bazafilmow;
+
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by Dawid Michałowicz on 19.04.2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
